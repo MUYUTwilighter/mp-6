1904378ba2eaa1ac7c1ffc814486793e98386d35
MUYU_Twilighter <1484605372@qq.com>
2025-04-28 02:08:08 -0400

    format

--- app/callback/page.tsx
+++ app/callback/page.tsx
@@ -1,19 +1,19 @@
 'use client'
-import { Suspense } from 'react'
-import { useSearchParams } from 'next/navigation'
-import { Container, Card, CardMedia, Typography } from '@mui/material'
+import {Suspense} from 'react'
+import {useSearchParams} from 'next/navigation'
+import {Container, Card, CardMedia, Typography} from '@mui/material'
 
 function CallbackContent() {
     const params = useSearchParams()
     return (
-        <Card sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', p: 2 }}>
+        <Card sx={{display: 'flex', flexDirection: 'row', alignItems: 'center', p: 2}}>
             <CardMedia
                 component="img"
                 src={params.get('avatar_url') ?? ''}
                 alt="avatar"
-                sx={{ width: 200, height: 200, borderRadius: '50%' }}
+                sx={{width: 200, height: 200, borderRadius: '50%'}}
             />
-            <Container sx={{ display: 'flex', flexDirection: 'column' }}>
+            <Container sx={{display: 'flex', flexDirection: 'column'}}>
                 <Typography variant="h6">Name: {params.get('name')}</Typography>
                 <Typography variant="h6">Email: {params.get('email')}</Typography>
             </Container>
@@ -26,7 +26,7 @@ export default function Callback() {
         <Suspense fallback={
             <Typography variant={"h4"}>Loading profile…</Typography>
         }>
-            <CallbackContent />
+            <CallbackContent/>
         </Suspense>
     )
 }
